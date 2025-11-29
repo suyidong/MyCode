8694267a902dcfda78981342ce6899e223b39712
suyidong <162973688+suyidong@users.noreply.github.com>
2025-11-29 17:49:45 +0800

    Update _init_.py

--- VAE-Catalyst-Project/models/_init_.py
+++ VAE-Catalyst-Project/models/_init_.py
@@ -1,1 +1,15 @@
+"""
+VAE Model Package
 
+This package contains the Variational Autoencoder model implementation
+for catalyst property prediction.
+"""
+
+from .vae_model import ImprovedVAE
+
+__all__ = [
+    'ImprovedVAE',
+]
+
+__version__ = '1.0.0'
+__author__ = 'Dong, Suyi'
