d5b6cc35f647bc9d042a3460f44b1677f39c0d31
suyidong <162973688+suyidong@users.noreply.github.com>
2025-11-29 17:51:10 +0800

    Create __init__.py

---
+++ VAE-Catalyst-Project/training/__init__.py
@@ -0,0 +1,15 @@
+"""
+Training Package
+
+This package contains the training scripts and utilities
+for the VAE Catalyst Project.
+"""
+
+from .train_vae import train_vae, main
+
+__all__ = [
+    'train_vae',
+    'main',
+]
+
+__version__ = '1.0.0'
